b55548ea73735ad6bf776cdc6d628a2f840c7e44
Ricky Pai <rickyp999+github@gmail.com>
2021-05-06 16:17:33 -0700

    toggle cells

--- pages/griddy/_cell.tsx
+++ pages/griddy/_cell.tsx
@@ -14,25 +14,42 @@ interface CellState {
 
   cellHeight: number;
   cellWidth: number;
+
+  cellActive: boolean;
 }
 
 class Cell extends React.Component<CellProps, CellState> {
   constructor(props: CellProps) {
     super(props);
 
-    this.state = { ...props }; // TODO: fix this
+    this.state = {
+      ...props,
+
+      cellActive: false,
+    }; // TODO: fix this
   }
 
   render(): JSX.Element {
+    const classes = ["cell"];
+    if (this.state.cellActive) {
+      classes.push("active");
+      console.log(classes);
+    }
     return (
+      // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
       <div
-        className="cell"
+        className={classes.join(" ")}
         style={{
           width: this.state.cellWidth,
           height: this.state.cellHeight,
           position: "absolute",
           marginTop: this.state.row * this.state.cellHeight,
           marginLeft: this.state.column * this.state.cellWidth,
+        }}
+        // role="switch"
+        onClick={() => {
+          this.setState((prevState) => ({ cellActive: !prevState.cellActive }));
+          console.log("click");
         }}
       >
         <p className="cell-controls">text</p>
